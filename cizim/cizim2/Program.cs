36552cca0242639574a0ea8f5aa4363bb8e93706
RÜVEYDA KEMİK <77358367+donduruveydakemiik@users.noreply.github.com>
2023-01-14 17:14:09 +0300

    Add files via upload

---
+++ cizim/cizim2/Program.cs
@@ -0,0 +1,105 @@
+﻿using cizim2.cizim;
+
+namespace cizim2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ilk örnek 
+            //sekil skl = new sekil();
+            //skl.byt.Yukseklik = 12;
+            //skl.byt.Genislik = 13;
+            //Console.WriteLine(" ");
+            //skl.pzyn.X = 5;
+            //skl.pzyn.Y = 6;
+            //skl.ciz(); ;
+
+            //sekil dksrtgn = new dikdortgen();
+            //dksrtgn.byt.Yukseklik = 22;
+            //dksrtgn.byt.Genislik = 23;
+            //Console.WriteLine(" ");
+            //dksrtgn.pzyn.X = 15;
+            //dksrtgn.pzyn.Y = 16;
+            //dksrtgn.ciz(); ;
+
+            #endregion
+
+
+            #region ikinci örnek
+            // program cs kısmında şekil çiz class oluşturuldu.
+
+            //var ikinci_ornek = new dikdortgen();
+            //ikinci_ornek.pzyn.X = 0;
+            //ikinci_ornek.pzyn.Y = 1;
+            //ikinci_ornek.byt.Yukseklik = 10;
+            //ikinci_ornek.byt.Genislik = 20;
+            //sekilciz(ikinci_ornek); 
+            #endregion
+
+
+            #region ucuncu örnek
+
+            //var ucuncu_ornek = new elips();
+            //ucuncu_ornek.pzyn.X = 0;
+            //ucuncu_ornek.pzyn.Y = 1;
+            //ucuncu_ornek.byt.Yukseklik = 10; 
+            //ucuncu_ornek.byt.Genislik = 20;
+            //sekilciz(ucuncu_ornek);
+            //ucuncu_ornek.tasi(new pozisyon { X = 100 ,Y=200});
+            //sekilciz(ucuncu_ornek);
+
+            #endregion
+
+
+            #region dorduncu örnek
+
+            //abstract sınıflar somutlaştırılamaz.
+            //sınıf veya metod abstract olabilir.
+            //yeni sınıflar üretilir ancak nesne üreilemez.
+            //override edilmesi zorunludur
+            //abstract metod varsa sınıfta abstract olmalıdır. 
+            //abstract sınıfta virtual otomatik olarak vardır.
+
+            //sekil s1= new dikdortgen();
+            //sekilciz(s1);
+
+            #endregion
+
+
+            #region besinci örnek
+            //sealed bir sınıftan yeni sınıflar türetilmesini önler
+            //sealed metod ise override edilemez artık
+            // string sınıfı sealed class yapısındadır.
+
+
+
+            #endregion
+
+
+            #region altıncı örnek
+
+            //yapılandırıcı sınıflar 
+
+            //sekil s1 = new dikdortgen();
+            //s1.byt.Genislik = 50;
+            //s1.byt.Yukseklik = 60;
+            //s1.pzyn.X = 1;
+            //s1.pzyn.Y = 2;
+
+
+            #endregion
+
+            #region yedinci örnek
+
+            sekil s1 = new dikdortgen(50, 50, 100, 100);
+            sekilciz(s1);
+            #endregion
+        }
+
+
+        public static void sekilciz(sekil sekli) =>
+            sekli.ciz(); //polymorphism
+
+    }
+}
---
+++ cizim/cizim2/cizim/sekil.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cizim2.cizim
+{
+    public sealed class pozisyon
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public override string? ToString() =>
+             $": x={X}, y={Y}";
+
+    }
+    public sealed class boyut
+    {
+        public int Genislik { get; set; }
+        public int Yukseklik { get; set; }
+
+        public override string? ToString() =>
+            $": Genislik= {Genislik}, Yukseklik={Yukseklik}";
+
+
+    }
+    public abstract class sekil
+    {
+        public sekil()
+        {
+            Console.WriteLine("base class yapılandırıcı sınıfı çalıştı.");
+        }
+
+        public sekil(int yukseklik, int genislik,int x,int y)
+        {
+            byt = new boyut{ Genislik = genislik, Yukseklik = yukseklik };
+            pzyn = new pozisyon { X = x, Y = y };
+        }
+
+
+
+
+        public pozisyon pzyn { get; } = new pozisyon();
+        public boyut byt { get; } = new boyut();
+        public virtual void ciz() =>
+            Console.WriteLine($"SEKIL " + $"\nPOZISYON= {pzyn}\nBOYUT=  {byt}");
+        public virtual void tasi(pozisyon yenipozisyon)
+        {
+            pzyn.X = yenipozisyon.X;
+            pzyn.Y = yenipozisyon.Y;
+            Console.WriteLine($" ŞEKİL TAŞINDI : x=  {pzyn.X}, y= {pzyn.Y}");
+        }
+
+        public abstract void yenidenboyutlandir(int genislik, int yukseklik);
+        
+       
+
+
+    }
+    public class dikdortgen : sekil
+    {
+        public dikdortgen()
+        {
+            Console.WriteLine("türetilmiş sınıf çalıştırıldı");
+        }
+
+        public dikdortgen(int yukseklik, int genislik, int x, int y) : base(yukseklik, genislik, x, y)
+        {
+            Console.WriteLine("türetilmiş sınıf 4 parameterli çalıştı ");
+        }
+
+        public override void ciz() =>
+            Console.WriteLine($"DIKDORGEN: POZISYON={pzyn}, BOYUT={byt}");
+
+        public override void yenidenboyutlandir(int genislik, int yukseklik)
+        {
+            byt.Yukseklik = yukseklik;
+            byt.Genislik= genislik;
+            throw new NotImplementedException();
+        }
+    }
+    public class elips : sekil
+    {
+        public override void tasi(pozisyon yenipozisyon)
+        {
+            Console.WriteLine("ELİPS TAŞINDI.");
+            base.tasi(yenipozisyon);
+        }
+
+        public override void yenidenboyutlandir(int genislik, int yukseklik)
+        {
+            byt.Yukseklik = yukseklik;
+            byt.Genislik = genislik;
+            throw new NotImplementedException();
+        }
+    }
+    
+}
